65c3c1cfe910fc56bba8d190e3de1ae7a753474e
kukuku <43928695+lkku1@users.noreply.github.com>
2024-04-10 10:49:30 +0800

    Update evaluate2.py

--- evaluate2.py
+++ evaluate2.py
@@ -108,6 +108,7 @@ def main():
             depth_max = outputs.max()
             max_val = (2 ** (8 * 1)) - 1
             depth_patch = (1 - (max_val * (outputs - depth_min) / (depth_max - depth_min)).int() / max_val) * 10.0
+          
             outputs = model(equi_inputs, depth_patch, roll_idx, flip)
 
             pred_depth = outputs["pred_depth"].detach().cpu()
